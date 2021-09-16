8223b1b1827eb247734366c230691544a559bbd2
Nadine <nadine.lisa.mathew@accenture.com>
2021-09-16 11:16:46 +0530

    styling-changes-issues

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let theme = localStorage.getItem('theme');
+
+if(theme === null){
+    setTheme('light');
+}else{
+    setTheme(theme);
+}
+
 let themeDots = document.getElementsByClassName('theme-dot');
 for(var i=0;themeDots.length>i;i++){
     themeDots[i].addEventListener('click',function(){
@@ -20,4 +28,5 @@ function setTheme(mode){
     if(mode==='purple'){
         document.getElementById('theme-style').href='purple.css';
     }
+    localStorage.setItem('theme', mode);
 }
